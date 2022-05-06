4dc19a09d564bca39c461f8192076db7baff1c23
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-05-06 16:17:06 +0200

    add faker generator

--- app/Train.php
+++ app/Train.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
-    //
+    protected $fillable = [
+        'agency',
+        'departure_station',
+        'arrival_station',
+        'departure_time',
+        'arrival_times',
+        'train_code',
+        'carriages',
+        'in_time',
+        'deleted',
+    ];
 }
---
+++ database/seeds/TrainSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Train;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 100; $i++) {
+            $trainData = [
+                'agency'            => $faker->company(),
+                'departure_station' => $faker->city(),
+                'arrival_station'   => $faker->city(),
+                'departure_time'    => $faker->dateTimeBetween('-1 day', '+1day'),
+                'arrival_times'     => $faker->dateTimeBetween('+1 day', '+2 day'),
+                'train_code'        => $faker->randomNumber(5),
+                'carriages'         => $faker->randomNumber(2),
+                'in_time'           => $faker->numberBetween(0, 1),
+                'deleted'           => $faker->numberBetween(0, 1),
+            ];
+
+            $train = new Train();
+            $train->fill($trainData);
+            $train->save();
+        }
+
+    }
+}
